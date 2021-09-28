1f37d7570fde4596053ff07e4541df08537450dc
Andrea santi <santi@MacBook-Air.homenet.telecomitalia.it>
2021-09-28 05:51:32 +0200

    fix small bug about infoInvestiment.module.css (wrong import) and split contact details in 2 more files

--- src/components/UI/InfoPageInvest/InfoInvestiments.js
+++ src/components/UI/InfoPageInvest/InfoInvestiments.js
@@ -1,20 +1,24 @@
 import React from "react";
 import up from "../../../images/UP.svg";
-//import classes from "./InfoInvestiments.module.css";
+import classes from "./InfoInvestiments.module.css";
 
 export default function InfoInvestiments(props) {
   const { text, nameInvest, roleInvest } = props;
   return (
-    <div className="info-investiments">
-      <span className="backtips-top"></span>
-      <span className="vector-left"></span>
-      <div className="info-investiments__text">{text}</div>
-      <div className="info-investiments__inv-container">
-        <div className="info-investiments__inv-name-container">
-          <span className="info-investiments__inv-name">{nameInvest}</span>
-          <span className="info-investiments__inv-name-role">{roleInvest}</span>
+    <div className={classes.info_investiments}>
+      <span className={classes["backtips-top"]}></span>
+      <span className={classes["vector-left"]}></span>
+      <div className={classes["info-investiments__text"]}>{text}</div>
+      <div className={classes["info-investiments__inv-container"]}>
+        <div className={classes["info-investiments__inv-name-container"]}>
+          <span className={classes["info-investiments__inv-name"]}>
+            {nameInvest}
+          </span>
+          <span className={classes["info-investiments__inv-name-role"]}>
+            {roleInvest}
+          </span>
         </div>
-        <span className="info-investiments__inv-name-logo">
+        <span className={classes["info-investiments__inv-name-logo"]}>
           <img src={up} alt="up-Logo" />
         </span>
       </div>
