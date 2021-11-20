d05fd38f94d20f017bcf4be746d9240a1f8d8e7e
Andrea santi <santi@MacBook-Air.local>
2021-11-20 22:16:05 +0100

    Add load data dinamically for Quotes Text

--- src/components/Layout/Sidebar/InvestitorQuotes/InvestitorQuotes.js
+++ src/components/Layout/Sidebar/InvestitorQuotes/InvestitorQuotes.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import up from "../../../../assets/images/UP.svg";
 import {
@@ -13,24 +13,60 @@ import {
   FooterLogoContainer,
   FooterLogo,
 } from "./InvestitorQuotes.style.js";
-import { investitorQuotesText } from "./InvestitorQuote.text.js";
+const axios = require("axios");
 
 export default function InfoInvestiments() {
   const location = useLocation();
+  const [quotes, setQuotes] = useState([
+    { titleQuote: "", roleQuote: "", investitorquotes: "" },
+  ]);
 
-  const { text, nameInvest, roleInvest } = investitorQuotesText(
-    location.pathname
-  );
+  useEffect(() => {
+    const getQuoteData = async () => {
+      const { data } = await axios.get(`http://localhost:1337/quote-texts`);
+      console.log(data);
+      setQuotes(data);
+
+      //add error handling and loading handling
+    };
+
+    getQuoteData();
+  }, []);
+
+  let titleQuoteMod = "";
+  let roleQuoteMod = "";
+  let investitorquotesMod = "";
+
+  if (location.pathname === "/") {
+    let { titleQuote, roleQuote, investitorquotes } = quotes[0];
+    titleQuoteMod = titleQuote;
+    roleQuoteMod = roleQuote;
+    investitorquotesMod = investitorquotes;
+  }
+
+  if (location.pathname === "/pages2") {
+    let { titleQuote, roleQuote, investitorquotes } = quotes[1];
+    titleQuoteMod = titleQuote;
+    roleQuoteMod = roleQuote;
+    investitorquotesMod = investitorquotes;
+  }
+
+  if (location.pathname === "/pages3") {
+    let { titleQuote, roleQuote, investitorquotes } = quotes[2];
+    titleQuoteMod = titleQuote;
+    roleQuoteMod = roleQuote;
+    investitorquotesMod = investitorquotes;
+  }
 
   return (
     <Wrapper>
       <Backtips></Backtips>
       <VectorLeft></VectorLeft>
-      <LongText>{text}</LongText>
+      <LongText>{investitorquotesMod}</LongText>
       <ContainerFooter>
         <ContainerQuotesCreator>
-          <QuoteCreator>{nameInvest}</QuoteCreator>
-          <QuoteCreatorRole>{roleInvest}</QuoteCreatorRole>
+          <QuoteCreator>{titleQuoteMod}</QuoteCreator>
+          <QuoteCreatorRole>{roleQuoteMod}</QuoteCreatorRole>
         </ContainerQuotesCreator>
         <FooterLogoContainer>
           <FooterLogo src={up} alt="up-Logo" />
